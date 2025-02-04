18d603eccd7c5a40726f3394291b6c4c83810a2d
vitthalRiyality <vitthal.riyality@gmail.com>
2025-02-04 16:09:46 +0530

    employee add

---
+++ Vaishnavi Garment/addEmployee.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Vaishnavi_Garment
+{
+    partial class addEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(281, 161);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(616, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Position";
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(801, 164);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(176, 26);
+            this.txtPosition.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Salary";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(281, 300);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(173, 26);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(616, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contact";
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(801, 294);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(176, 26);
+            this.txtContact.TabIndex = 7;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(281, 373);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(111, 56);
+            this.saveBtn.TabIndex = 8;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 688);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "addEmployee";
+            this.Text = "addEmployee";
+            this.Load += new System.EventHandler(this.addEmployee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
---
+++ Vaishnavi Garment/addEmployee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Vaishnavi_Garment
+{
+    public partial class addEmployee : Form
+    {
+        string dbPath = System.IO.Path.Combine(Application.StartupPath, "vaishnavi.accdb");
+        string connectionString;
+        public addEmployee()
+        {
+            InitializeComponent();
+            connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + dbPath + "';";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string empName = txtName.Text.Trim();
+            string empPosition = txtPosition.Text.Trim();
+            string empSalary = txtSalary.Text.Trim();
+            string empContact = txtContact.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(empName) || string.IsNullOrWhiteSpace(empPosition) ||
+                string.IsNullOrWhiteSpace(empSalary) || string.IsNullOrWhiteSpace(empContact))
+            {
+                MessageBox.Show("Please fill all fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                // Use [position] in square brackets to avoid syntax errors
+                string query = "INSERT INTO Employee (name, [position], salary, contact) VALUES (@Name, @Position, @Salary, @Contact)";
+                OleDbCommand command = new OleDbCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", empName);
+                command.Parameters.AddWithValue("@Position", empPosition);
+                command.Parameters.AddWithValue("@Salary", Convert.ToDecimal(empSalary));
+                command.Parameters.AddWithValue("@Contact", empContact);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Employee added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtName.Clear();
+                    txtPosition.Clear();
+                    txtSalary.Clear();
+                    txtContact.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void addEmployee_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
